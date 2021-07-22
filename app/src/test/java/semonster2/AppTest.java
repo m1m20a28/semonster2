223bc2cdd29e61c1d5320f6952819e39898913b3
Azutras <coppsz3@outlook.jp>
2021-07-22 16:06:00 +0900

    fixed error

--- app/src/test/java/semonster2/AppTest.java
+++ app/src/test/java/semonster2/AppTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new Monster();
-        assertNotNull("魔王:レア度[10]", classUnderTest.toString());
-    }
+  @Test
+  public void testAppHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 }
