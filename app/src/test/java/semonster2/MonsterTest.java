7c92834da7a591f671dab1312778adbf04d2dbfe
Tanimura-Takumi <m1m21a30@oit.ac.jp>
2021-07-25 00:13:28 +0900

    add rareupdn

--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -6,7 +6,8 @@ import static org.junit.Assert.*;
 public class MonsterTest {
   @Test
   public void testMonster() {
-    Monster monster = new Monster(4,10);
-    assertEquals("魔王:レア度[10]", monster.toString());
+    Monster monster = new Monster(4,10, 8);
+    assertEquals("魔王:レア度[10]のレア度を[8]に変更", monster.updwrareString());
+    assertEquals("魔王:レア度[8]", monster.truerareString());
   }
 }
